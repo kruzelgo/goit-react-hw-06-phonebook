197032156aab27e8413bf84be1f543e8d734635a
kruzelgo <kruzelgo@gmail.com>
2024-05-19 20:09:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
 import App from './components/App.jsx';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
